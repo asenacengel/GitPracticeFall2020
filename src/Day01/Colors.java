aacad5f0c093389a71005f2cf87321918312ece3
asenacengel <57203106+asenacengel@users.noreply.github.com>
2020-03-25 20:22:31 -0400

    new fruit

--- src/Day01/Colors.java
+++ src/Day01/Colors.java
@@ -3,7 +3,12 @@ package Day01;
 public class Colors {
     public static void main(String[] args) {
 
-        String[] colors ={"Red","blue","green","yellow"};
+        String a = "interned String";
+        String b = new String("interned String").intern();
+        String c = b.intern();
+        System.out.println(b==c);
+        System.out.println(a==c);
+        System.out.println(b==a);
 
     }
 }
